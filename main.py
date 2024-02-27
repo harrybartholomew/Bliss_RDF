caa6a6d9c611676384f3989aca5bf037c154e3e4
Harry Bartholomew <harry.bartholomew@gmail.com>
2024-02-27 16:59:21 +0000

    Restructure

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from rdflib.namespace import SKOS
 ex = Namespace("http://example.org/")
 g = Graph()
 
-def create_node(counter, line):
+
+def create_node(uri, lines):
+
+    this_line = lines[0]
+
     def extract_classmark(s):
         classmark = s.split(" ")[0]
         if classmark == "@":
@@ -12,18 +16,37 @@ def create_node(counter, line):
         else:
             return classmark
 
-    def extract_label(s):
-        words = s.split()
-        excluded_notation = " ".join(words[1:])
-        return excluded_notation[2:]
+    def extract_label(lines):
 
-    uri = ex[f"{counter}"]
-    label = extract_label(line)
-    if label:
-        g.add((uri, RDF.type, SKOS.Concept))
-        g.add((uri, SKOS.prefLabel, Literal(label)))
+        def add_to_label(label, new_line):
 
-    notation = extract_classmark(line)
+            def remove_excess_whitespace(string):
+                words = string.split()
+                return " ".join(words)
+
+            label += f" {remove_excess_whitespace(new_line)}"
+            return label
+
+        first_line_words = this_line.split()
+        label = " ".join(first_line_words[1:])[2:]
+        more_labels = True
+        lines_ahead = 1
+        while more_labels:
+            if len(lines) > 1:
+                next_line = lines[lines_ahead]
+                if identify_line(next_line) == "continued":
+                    label = add_to_label(label, next_line)
+                    lines_ahead += 1
+                else:
+                    more_labels = False
+            else:
+                more_labels = False
+        return label
+
+    label = extract_label(lines)
+    g.add((uri, RDF.type, SKOS.Concept))
+    g.add((uri, SKOS.prefLabel, Literal(label)))
+    notation = extract_classmark(this_line)
     if notation:
         g.add((uri, SKOS.notation, Literal(notation)))
 
@@ -32,24 +55,27 @@ def extract_indentation(line):
     return int(line.split()[1][:2])
 
 
-def identify_line(line):
+def identify_line(line): #some nodes start with without @ sign
     if line[0] != " ":
         return "new node"
     if line.strip()[0] == "*":
         return "new note"
     else:
-        return "continued note"
+        return "continued"
 
 
 def parse_source_file(file_path):
     with open(file_path, 'r') as file:
-        counter = 0
-        for line in file:
-            line_type = identify_line(line)
+        counter = 1
+        lines = file.readlines()
+        for i in range(len(lines)):
+            line_type = identify_line(lines[i])
             if line_type == "new node":
-                create_node(counter, line)
+                uri = ex[f"{str(counter).zfill(4)}"]
+                create_node(uri, lines[i:])
                 counter += 1
 
 
 parse_source_file("source_code.txt")
+g.serialize(destination="tbl.ttl")
 print(g.serialize())
